ca5fa20f052b98a0f9f98f708cc09656d21be4d6
BimboBabatola <bimbo@woodcoreapp.com>
2023-09-24 14:24:48 +0100

    update pymongo version

--- mongodb_connection.py
+++ mongodb_connection.py
@@ -1,34 +1,31 @@
 from dotenv import load_dotenv
 import os
-
-from pymongo import MongoClient
-from pymongo.server_api import ServerApi
-import ssl
+from pymongo.mongo_client import MongoClient
 
 load_dotenv()
 
 
 MONGODB_USER = os.environ['MONGODB_USER']
 MONGODB_PASSWORD = os.environ['MONGODB_PASSWORD']
-MONGODB_URI = f"mongodb+srv://{MONGODB_USER}:{MONGODB_PASSWORD}@myatlasclusteredu.xncbdmr.mongodb.net/?retryWrites=true&w=majority"
+
 
 class MongoDBConnection:
     def __init__(self) -> None:
-        pass
+        self.MONGODB_URI = f"mongodb+srv://{MONGODB_USER}:{MONGODB_PASSWORD}@redditcluster.hnctuvt.mongodb.net/?retryWrites=true&w=majority"
 
     def client(self):
         # Create a new client and connect to the server
-        client = MongoClient(MONGODB_URI, server_api=ServerApi('1'), ssl_cert_reqs=ssl.CERT_NONE)
+        client = MongoClient(self.MONGODB_URI)
         return client
 
     def test_connection(self):
         # Send a ping to confirm a successful connection
-        client = self.client()
+        client = MongoClient(self.MONGODB_URI)
         try:
             client.admin.command('ping')
             print("Pinged your deployment. You successfully connected to MongoDB!")
         except Exception as e:
-            print(e)
+            print('could not test connection ', e)
 
     def load_reddit_data(self, data):
         client = self.client()
@@ -39,3 +36,5 @@ class MongoDBConnection:
             
         except Exception as e:
             print('Could not load data into DB, ', e)
+
+MongoDBConnection().test_connection()
