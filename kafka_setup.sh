cadd5741ac9961ec4205c69e18019a82126c17d3
BimboBabatola <bimbo@woodcoreapp.com>
2023-09-27 03:27:09 +0100

    created env var for kafka topic

--- kafka_setup.sh
+++ kafka_setup.sh
@@ -3,23 +3,23 @@
 source .env
 
 # # Build and start Docker containers
-# docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose.yml up -d
 
 # # Wait for Kafka to be ready
-# until [ $(docker-compose logs kafka | grep -c 'started (kafka.server.KafkaServer)') -gt 0 ]; do
-#   echo "Waiting for Kafka to start..."
-#   sleep 1
-# done
+until [ $(docker-compose logs kafka | grep -c 'started (kafka.server.KafkaServer)') -gt 0 ]; do
+  echo "Waiting for Kafka to start..."
+  sleep 1
+done
 
 # #Connect to Kafka shell
-# docker exec -it kafka /bin/sh
+docker exec -it kafka /bin/sh
 
 # # Create Kafka topic
 echo "Creating Kafka topic > $KAFKA_TOPIC "
-# kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic $KAFKA_TOPIC
+kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic $KAFKA_TOPIC
 
 
-# echo "Kafka setup complete."
+echo "Kafka setup complete."
 
 
 
